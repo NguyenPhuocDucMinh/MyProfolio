f43ae1e551347a03a157ede2870fe7a4d1082a7c
unknown <ducminh852005@gmail.com>
2025-03-04 22:10:59 +0700

    Upload website

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleButton = document.querySelector(".menu-toggle");
+if (toggleButton) {
+    toggleButton.addEventListener("click", function() {
+        document.querySelector(".menu").classList.toggle("active");
+    });
+} else {
+    console.error("Không tìm thấy .menu-toggle");
+}
